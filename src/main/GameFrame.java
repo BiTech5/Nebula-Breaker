edf45d9d69a9a0fe297b1bdab1586142757dc315
Bikram <bikikc7@gmail.com>
2025-05-19 11:47:17 +0545

    init 0

---
+++ src/main/GameFrame.java
@@ -0,0 +1,7 @@
+package src.main;
+
+public class GameFrame {
+    public static void main(String[] args){
+        System.out.println("Game Frame Here");
+    }
+}
---
+++ src/main/_Main.java
@@ -0,0 +1,7 @@
+package src.main;
+
+public class _Main {
+    public static void main(String[] args){
+        System.out.println("Main code here");
+    }
+}
