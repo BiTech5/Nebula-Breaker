2b42b3858112a557de5e7a7261af31e81eaa4534
Bikram <bikikc7@gmail.com>
2025-06-22 17:19:01 +0545

    Implement mute toggle button to control background music

--- src/util/GameFrame.java
+++ src/util/GameFrame.java
@@ -1,18 +1,21 @@
 package src.util;
 // import java.awt.Color;
-import javax.swing.JFrame;
-import src.view.HomePage;
-import src.util.SoundManager;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+import src.core.PlayMusic;
+import src.view.HomePage;
 public class GameFrame extends JFrame {
     public int width=350;
     public int height = 600;
     public GameFrame() {
         // adding game logo
-        SoundManager.playBackgroundMusic("assets/sounds/music/background_music.wav");
+        
         ImageIcon logo = new ImageIcon("assets/images/logo.png");
         this.setIconImage(logo.getImage());
 
+        PlayMusic musicPlayer = new PlayMusic(); 
+        musicPlayer.play(true);
         this.setTitle("Nebula Breaker");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(width,height);
--- src/view/SettingsPage.java
+++ src/view/SettingsPage.java
@@ -1,4 +1,5 @@
 package src.view;
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
@@ -6,7 +7,10 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import src.core.PageNavigator;
+import src.core.PlayMusic;
+
 public class SettingsPage extends JPanel {
+
     private JLabel l1;
     private JButton home;
     private Image backgroundImage;
@@ -18,31 +22,30 @@ public class SettingsPage extends JPanel {
     private JLabel musicLabel;
     private JToggleButton toggleMusicBtn;
 
-    public SettingsPage(int fr_width, int fr_height){
+    public SettingsPage(int fr_width, int fr_height) {
         setLayout(null);
         setPreferredSize(new Dimension(fr_width, fr_height));
 
         audio = new JLabel("AUDIO");
         musicLabel = new JLabel("MUSIC");
 
-        try{
-        BufferedImage bgImage = ImageIO.read(new File("assets/images/nebula_breaker_bg_img.png"));
-        backgroundImage = bgImage.getScaledInstance(fr_width, fr_height, Image.SCALE_SMOOTH);
-     } catch(IOException e){
-        System.out.println("Error loading background image" + e.getMessage());
-     }
+        try {
+            BufferedImage bgImage = ImageIO.read(new File("assets/images/nebula_breaker_bg_img.png"));
+            backgroundImage = bgImage.getScaledInstance(fr_width, fr_height, Image.SCALE_SMOOTH);
+        } catch (IOException e) {
+            System.out.println("Error loading background image" + e.getMessage());
+        }
 
         //back to home button
         home = new JButton();
         ImageIcon leftArrow = new ImageIcon("assets/images/left_arrow.png");
         Image arrowImg = leftArrow.getImage();
-        Image scaleImage = arrowImg.getScaledInstance(18,18,Image.SCALE_SMOOTH);
+        Image scaleImage = arrowImg.getScaledInstance(18, 18, Image.SCALE_SMOOTH);
         ImageIcon scaledArrow = new ImageIcon(scaleImage);
 
         int buttonWidth = 180;
         int buttonHeight = 40;
 
-        
         int x_home = (fr_width - buttonWidth) / 2;
         int y_home = 30;
 
@@ -55,8 +58,8 @@ public class SettingsPage extends JPanel {
         home.setIcon(scaledArrow);
         home.setIconTextGap(8);
         home.setOpaque(true);
-        home.setBorder(BorderFactory.createLineBorder(Color.decode("#00D8FF"),2));
-        home.addActionListener(e->{
+        home.setBorder(BorderFactory.createLineBorder(Color.decode("#00D8FF"), 2));
+        home.addActionListener(e -> {
             PageNavigator.navigateTo(this, new HomePage(fr_width, fr_height));
             backgroundImage = null;
         });
@@ -68,23 +71,22 @@ public class SettingsPage extends JPanel {
         l1.setFont(new Font("SansSerrif", Font.BOLD, 26));
         int labelWidth = 150;
         int labelHeight = 40;
-        int y_title = y_home + buttonHeight + 10; 
+        int y_title = y_home + buttonHeight + 10;
         int x_title = (fr_width - labelWidth) / 2;
         l1.setBounds(x_title, y_title, labelWidth, labelHeight);
 
         //Audio
         audioBox = new JPanel();
         audioBox.setOpaque(true);
-        audioBox.setBackground(new Color(0,0,50,(int)(0.5 * 255)));
+        audioBox.setBackground(new Color(0, 0, 50, (int) (0.5 * 255)));
         audioBox.setBorder(BorderFactory.createLineBorder(Color.decode("#00D8FF"), 1));
         audioBox.setLayout(null);
-        
+
         int audioBoxWidth = 230;
         int audioBoxHeight = 90;
-        int x_audioBox = ((fr_width - audioBoxWidth)/2) - 10;
-        int y_audioBox = (fr_height - audioBoxHeight - 300) -70;
+        int x_audioBox = ((fr_width - audioBoxWidth) / 2) - 10;
+        int y_audioBox = (fr_height - audioBoxHeight - 300) - 70;
         audioBox.setBounds(x_audioBox, y_audioBox, audioBoxWidth, audioBoxHeight);
-
 
         //audio styling
         audio.setForeground(Color.decode("#A0F8FF"));
@@ -92,7 +94,7 @@ public class SettingsPage extends JPanel {
 
         int audioWidth = 150;
         int audioHeight = 40;
-        int x_audioTitle = ((audioBoxWidth - audioWidth)/2);
+        int x_audioTitle = ((audioBoxWidth - audioWidth) / 2);
         int y_audioTitle = 5;
         audio.setBounds(x_audioTitle, y_audioTitle, audioWidth, audioHeight);
 
@@ -109,39 +111,36 @@ public class SettingsPage extends JPanel {
         toggleMusicBtn.setBackground(Color.decode("#0D0D0D"));
         toggleMusicBtn.setOpaque(true);
         toggleMusicBtn.setFocusPainted(false);
-        toggleMusicBtn.setBorder(BorderFactory.createLineBorder(Color.decode("#00D8FF"),1));
+        toggleMusicBtn.setBorder(BorderFactory.createLineBorder(Color.decode("#00D8FF"), 1));
 
         int musicBtnWidth = 50;
         int musicBtnHeight = 25;
-        int x_musicBtn = ((audioBoxWidth - musicBtnWidth)/2) + 70;
+        int x_musicBtn = ((audioBoxWidth - musicBtnWidth) / 2) + 70;
         int y_musicBtn = 50;
         toggleMusicBtn.setBounds(x_musicBtn, y_musicBtn, musicBtnWidth, musicBtnHeight);
 
+        PlayMusic musicPlayer = new PlayMusic();
         toggleMusicBtn.addActionListener(e -> {
-            if(toggleMusicBtn.isSelected()){
+            if (toggleMusicBtn.isSelected()) {
                 toggleMusicBtn.setText("OFF");
-            }else{
+                musicPlayer.stop();
+            } else {
                 toggleMusicBtn.setText("ON");
+                musicPlayer.play(true);
             }
         });
-        
-
 
         //Main box
         mainBox = new JPanel();
         int mainBoxWidth = 250;
         int mainBoxHeight = 400;
-        int x_box = ((fr_width - mainBoxWidth) / 2)-10;
+        int x_box = ((fr_width - mainBoxWidth) / 2) - 10;
         int y_box = fr_height - mainBoxHeight - 70;
         mainBox.setBounds(x_box, y_box, mainBoxWidth, mainBoxHeight);
         mainBox.setOpaque(false);
         mainBox.setBorder(BorderFactory.createLineBorder(Color.decode("#00D8FF"), 2));
         mainBox.setBackground(Color.WHITE);
         audio.setHorizontalAlignment(SwingConstants.CENTER);
-
-
-        
-
 
         audioBox.add(audio);
         audioBox.add(musicLabel);
@@ -150,17 +149,16 @@ public class SettingsPage extends JPanel {
         add(home);
         add(mainBox);
         add(audioBox);
-        
 
-        
     }
+
     @Override
-        protected void paintComponent(Graphics g){
-            super.paintComponent(g);
-            if (backgroundImage != null) {
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
             g.drawImage(backgroundImage, 0, 0, this);
         }
 
-        }
+    }
 
 }
